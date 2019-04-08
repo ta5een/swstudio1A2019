0140feb89c9c44bf8795fbfa49c53dfb46559b12
Jakeb Gutierrez <drwaffle69@gmail.com>
2019-04-08 14:57:35 +1000

    added a barebones login and sign up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fire from './config/Fire';
+import Home from './components/Home';
+import Login from './components/Login';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            SES1A App
-            Commit Test
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </header>
+        {this.state.user ? (
+          <Home />
+        ) :
+          (
+            <Login />
+          )}
       </div>
     );
   }
---
+++ src/config/Fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDX-KA5IcmpfcwC26by5dksCrtiQcfqBwA",
+    authDomain: "swstudio1a2019.firebaseapp.com",
+    databaseURL: "https://swstudio1a2019.firebaseio.com",
+    projectId: "swstudio1a2019",
+    storageBucket: "swstudio1a2019.appspot.com",
+    messagingSenderId: "758333657659"
+};
+const fire = firebase.initializeApp(config);
+export default fire;
