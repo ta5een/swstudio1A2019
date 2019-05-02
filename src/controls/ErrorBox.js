a9491f7407921e494f0ae57b85f224c4a69e7af0
tahscenery <tahscenery@gmail.com>
2019-05-03 02:12:54 +1000

    New Error Box component

---
+++ src/controls/ErrorBox.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Defaults from '../AppDefaults';
+
+const ErrorBox = styled.div`
+  background: #d14040;
+  border: 0.5px solid #D4242C;
+  border-radius: 5px;
+  color: white;
+  padding: 2px 10px;
+
+  text-align: left;
+`;
+
+export { ErrorBox };
