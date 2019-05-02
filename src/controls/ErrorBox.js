881b2a78881f1aea983c642ea7ee01729d2c51b1
tahscenery <tahscenery@gmail.com>
2019-05-03 02:46:24 +1000

    Assigned colours with constants

--- src/controls/ErrorBox.js
+++ src/controls/ErrorBox.js
@@ -1,14 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Defaults from '../AppDefaults';
 
+var ErrorBoxClr = Object.freeze({
+  default: {
+    background: '#D14040',
+    border: '#D4242C'
+  },
+  warning: {
+    background: '#F99F5E',
+    border: '#E29258'
+  }
+});
+
 const ErrorBox = styled.div`
-  background: #d14040;
-  border: 0.5px solid #D4242C;
+  background: ${ErrorBoxClr.default.background};
+  border: 0.5px solid ${ErrorBoxClr.default.border};
   border-radius: 5px;
   color: white;
   padding: 2px 10px;
 
   text-align: left;
+  font-weight: 600;
+
+  ${props => props.warning && css`
+    background: ${ErrorBoxClr.warning.background};
+    border: 0.5px solid ${ErrorBoxClr.warning.border};
+  `}
 `;
 
 export { ErrorBox };
