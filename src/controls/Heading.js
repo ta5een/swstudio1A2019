dea20df6dfbed55a40690b8b5190236f550997e6
tahscenery <tahscenery@gmail.com>
2019-05-02 23:48:22 +1000

    New Heading component

---
+++ src/controls/Heading.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Defaults from '../AppDefaults';
+
+const Heading = styled.h1`
+  font-size: ${Defaults.constants.font.sizes.heading}
+`;
+
+export { Heading };
