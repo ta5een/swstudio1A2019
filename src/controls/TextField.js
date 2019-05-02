e977e2b30f93b3ce818c745530c4adad1069610a
tahscenery <tahscenery@gmail.com>
2019-05-03 01:04:12 +1000

    Handling WebKit styling

--- src/controls/TextField.js
+++ src/controls/TextField.js
@@ -8,6 +8,9 @@ const TextField = styled.input`
   border: 0.5px solid grey;
   padding: 0.8em;
 
+  // Prevents custom iOS styling
+  -webkit-appearance: none;
+
   font-size: ${textFieldFontSize};
 `;
 
