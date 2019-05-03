5e753e3472cc60bf75212e57e980fce602094a20
tahscenery <tahscenery@gmail.com>
2019-05-03 18:40:17 +1000

    Added title element

--- src/controls/Heading.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import Defaults from '../AppDefaults';
-
-const Heading = styled.h1`
-  font-size: ${Defaults.constants.font.sizes.heading}
-`;
-
-export { Heading };
---
+++ src/controls/Headings.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Defaults from '../AppDefaults';
+
+const Title = styled.h1`
+  font-size: ${Defaults.constants.font.sizes.title};
+`;
+
+const H1 = styled.h1`
+  font-size: ${Defaults.constants.font.sizes.heading};
+`;
+
+export { Title, H1 };
