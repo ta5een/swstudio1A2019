bca8b6dc304cc69aad03e88a92116539e89619ac
tahscenery <tahscenery@gmail.com>
2019-05-03 22:25:57 +1000

    Edited text field border colour and thickness

--- src/controls/TextField.js
+++ src/controls/TextField.js
@@ -4,7 +4,7 @@ import Defaults from '../AppDefaults';
 const textFieldFontSize = Defaults.constants.font.sizes.normal;
 
 const TextField = styled.input`
-  border: 0.5px solid grey;
+  border: 1px solid #848484;
   border-radius: 5px;
   padding: 0.8em;
 
