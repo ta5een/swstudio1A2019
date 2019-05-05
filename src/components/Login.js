5b9b0263bfd5e6b7314b3dd46ac6601e625e639d
tahscenery <tahscenery@gmail.com>
2019-05-05 14:21:38 +1000

    Refactored to use UI module

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,9 @@ import fire from '../config/Fire';
 import AppDefaults from '../AppDefaults';
 import './Login.css';
 
-import { Button, HintButton } from '../controls/Button';
-import { TextField } from '../controls/TextField';
-import { Title } from '../controls/Headings';
-import { InfoBox, WarningBox, ErrorBox } from '../controls/InfoBox';
+import * as UI from '../controls/UI';
 
-const InfoBoxType = Object.freeze({
+const DialogType = Object.freeze({
   DEFAULT: 0,
   WARNING: 1,
   ERROR: 2
@@ -69,15 +66,15 @@ class Login extends Component {
     var passwordTextFieldLength = document.getElementById('passwordTextField').value.length;
 
     if (emailTextFieldLength === 0 && passwordTextFieldLength === 0) {
-      this.showInfoBox("Please enter your email and password.", InfoBoxType.WARNING);
+      this.showInfoBox("Please enter your email and password.", DialogType.WARNING);
       this.setState({ errorEmail: true, errorPassword: true });
       document.getElementById('emailTextField').focus();
     } else if (emailTextFieldLength === 0) {
-      this.showInfoBox("Please enter your email.", InfoBoxType.WARNING);
+      this.showInfoBox("Please enter your email.", DialogType.WARNING);
       this.setState({ errorEmail: true });
       document.getElementById('emailTextField').focus();
     } else if (passwordTextFieldLength === 0) {
-      this.showInfoBox("Please enter your password.", InfoBoxType.WARNING);
+      this.showInfoBox("Please enter your password.", DialogType.WARNING);
       this.setState({ errorPassword: true });
       document.getElementById('passwordTextField').focus();
     } else {
@@ -97,19 +94,19 @@ class Login extends Component {
 
     switch (error.code) {
       case 'auth/invalid-email':
-        this.showInfoBox("Hmm, that email doesn't look right. Check that you've entered it correctly and try again.", InfoBoxType.ERROR);
+        this.showInfoBox("Hmm, that email doesn't look right. Check that you've entered it correctly and try again.", DialogType.ERROR);
         this.setState({ errorEmail: true });
         document.getElementById('emailTextField').focus();
         break;
       case 'auth/user-not-found': // Fallthrough
       case 'auth/wrong-password':
-        this.showInfoBox("Something's not right... Perhaps you've entered your email or password incorrectly?", InfoBoxType.ERROR);
+        this.showInfoBox("Something's not right... Perhaps you've entered your email or password incorrectly?", DialogType.ERROR);
         break;
       case 'auth/user-disabled':
-        this.showInfoBox("Seems like this account has been disabled. Please contact support for more information.", InfoBoxType.ERROR);
+        this.showInfoBox("Seems like this account has been disabled. Please contact support for more information.", DialogType.ERROR);
         break;
       case 'auth/too-many-requests':
-        this.showInfoBox("Woah, slow down! Look's like you've requested too many requests.", InfoBoxType.ERROR);
+        this.showInfoBox("Woah, slow down! Look's like you've requested too many requests.", DialogType.ERROR);
         break;
       default:
         this.showInfoBox(error.message);
@@ -117,18 +114,18 @@ class Login extends Component {
     }
   }
 
-  showInfoBox(message, type=InfoBoxType.DEFAULT) {
+  showInfoBox(message, type=DialogType.DEFAULT) {
     var infoBoxDiv = document.getElementById('infoBoxDiv');
 
     switch (type) {
       case 1:
-        ReactDOM.render(<WarningBox>{message}</WarningBox>, infoBoxDiv);
+        ReactDOM.render(<UI.WarningBox>{message}</UI.WarningBox>, infoBoxDiv);
         break;
       case 2:
-        ReactDOM.render(<ErrorBox>{message}</ErrorBox>, infoBoxDiv);
+        ReactDOM.render(<UI.ErrorBox>{message}</UI.ErrorBox>, infoBoxDiv);
         break;
       default:
-        ReactDOM.render(<InfoBox>{message}</InfoBox>, infoBoxDiv);
+        ReactDOM.render(<UI.InfoBox>{message}</UI.InfoBox>, infoBoxDiv);
     }
 
     infoBoxDiv.hidden = false;
@@ -169,26 +166,26 @@ class Login extends Component {
       <div className="wrapper">
         <div className="form-container">
           <div className="title-group">
-            <Title>{AppDefaults.app.name}</Title>
+            <UI.Title>{AppDefaults.app.name}</UI.Title>
             <p className="caption">{AppDefaults.app.caption}</p>
           </div>
           <form>
             <div className="form-group">
               <label htmlFor="form-group" className={this.state.errorEmail ? "error-text-field-label" : null}>email</label>
-              <TextField id="emailTextField" className={this.state.errorEmail ? "error-text-field" : null} name="email" type="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailTextFieldChange.bind(this)} onKeyPress={focusPasswordField.bind(this)} noValidate/>
+              <UI.TextField id="emailTextField" className={this.state.errorEmail ? "error-text-field" : null} name="email" type="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailTextFieldChange.bind(this)} onKeyPress={focusPasswordField.bind(this)} noValidate/>
             </div>
             <div className="form-group">
               <label htmlFor="form-group" className={this.state.errorPassword ? "error-text-field-label" : null}>password</label>
-              <TextField id="passwordTextField" className={this.state.errorPassword ? "error-text-field" : null} name="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordTextFieldChange.bind(this)} onKeyPress={submitForm.bind(this)} noValidate/>
+              <UI.TextField id="passwordTextField" className={this.state.errorPassword ? "error-text-field" : null} name="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordTextFieldChange.bind(this)} onKeyPress={submitForm.bind(this)} noValidate/>
             </div>
           </form>
           <div id="infoBoxDiv" className="info-box-div" hidden={true}/>
           <div>
             <div className="button-group">
-              <Button id="signUpButton" className="sign-up-button" type="button" disabled={!isEnabled} onClick={this.handleSignUp}>Sign up</Button>
-              <Button primary id="loginButton" type="submit" disabled={!isEnabled} onClick={this.handleLogin}>Login</Button>
+              <UI.Button id="signUpButton" className="sign-up-button" type="button" disabled={!isEnabled} onClick={this.handleSignUp}>Sign up</UI.Button>
+              <UI.Button primary id="loginButton" type="submit" disabled={!isEnabled} onClick={this.handleLogin}>Login</UI.Button>
             </div>
-            <HintButton type="button" onClick={this.forgotPassword}>Forgot your password?</HintButton>
+            <UI.HintButton type="button" onClick={this.forgotPassword}>Forgot your password?</UI.HintButton>
           </div>
         </div>
       </div>
