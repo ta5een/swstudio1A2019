8f54b24f38cf71bedad420ad646274d4573a2572
tahscenery <tahscenery@gmail.com>
2019-05-05 19:05:22 +1000

    Added font properties

--- src/AppDefaults.js
+++ src/AppDefaults.js
@@ -6,8 +6,18 @@ const AppDefaults = Object.freeze({
   },
   constants: {
     font: {
+      imports: {
+        all: `
+          @import url('https://fonts.googleapis.com/css?family=Rubik:400,500,700');
+        `,
+        rubik: "@import url('https://fonts.googleapis.com/css?family=Rubik:400,500,700');"
+      },
+      family: {
+        default: "'Rubik', sans-serif"
+      },
       sizes: {
         normal: "16px",
+        caption: "24px",
         heading: "50px",
         title: "70px"
       }
