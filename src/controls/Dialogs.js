8d01443bd1dac9b8a7cda2201491ade17e52a46c
tahscenery <tahscenery@gmail.com>
2019-05-05 14:42:10 +1000

    Removed unused imports

--- src/controls/Dialogs.js
+++ src/controls/Dialogs.js
@@ -1,5 +1,4 @@
-import styled, { css } from 'styled-components';
-import AppDefaults from '../AppDefaults';
+import styled from 'styled-components';
 
 var InfoBoxClr = Object.freeze({
   default: {
