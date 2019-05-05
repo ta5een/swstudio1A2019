efc2c405d226884a4bed81ae7061637a944003f5
tahscenery <tahscenery@gmail.com>
2019-05-05 14:21:27 +1000

    Added centralised UI module

---
+++ src/controls/UI.js
@@ -0,0 +1,19 @@
+import { Title, H1 } from './Headings';
+
+import { Button, HintButton } from './Buttons';
+import { TextField } from './TextField';
+
+import { InfoBox, WarningBox, ErrorBox } from './Dialogs';
+
+export { 
+  Title,
+  H1,
+
+  Button,
+  HintButton,
+  TextField,
+
+  InfoBox,
+  WarningBox,
+  ErrorBox
+};
