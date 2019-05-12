6ac9c4cea1c7a8e9203edf5c4dd004c842b91440
tahscenery <tahscenery@gmail.com>
2019-05-13 01:09:12 +1000

    Added page routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(
-    <App />, document.getElementById('root'));
+import App from './App';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
+
+import './index.css';
+
+const history = createBrowserHistory();
+
+const routes = (
+  <BrowserRouter history={history}>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/sign-up" component={SignUp}/>
+      <Route path="/home" component={Home}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
