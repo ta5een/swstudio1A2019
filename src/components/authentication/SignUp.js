d23d0f180dd9df9683646a485a4bff42f780dd32
tahscenery <tahscenery@gmail.com>
2019-05-13 23:29:16 +1000

    Added account creation functionality

--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, withRouter } from 'react-router-dom';
 
 import fire from '../../config/Fire';
 import AppDefaults from '../../AppDefaults';
@@ -32,6 +33,10 @@ class SignUp extends Component {
 
   componentDidMount() {
     document.title = `${AppDefaults.app.name} – Sign Up`;
+  }
+
+  componentWillReceiveProps(props) {
+
   }
 
   handleCreateAccount(e) {
@@ -84,23 +89,54 @@ class SignUp extends Component {
 
   commitCreateAccount() {
     this.showInfoBox("Creating account...");
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((user) => {
+      this.props.history.push('/account-details');
+    }).catch((error) => {
+      this.displayAuthError(error);
+    })
   }
 
-  showInfoBox(message, type=DialogType.DEFAULT) {
+  displayAuthError(error) {
+    switch (error.code) {
+      case 'auth/invalid-email':
+        this.showInfoBox("Hmm, that email doesn't look right. Check that you've entered it correctly and try again.", DialogType.ERROR);
+        this.setState({ errorEmail: true });
+        document.getElementById('emailTextField').focus();
+        break;
+      case 'auth/email-already-in-use':
+        this.showInfoBox("The email you provided already exists.", DialogType.ERROR, { description: "Forgot your password?", page: '/forgot-password'});
+        this.setState({ errorEmail: true });
+        document.getElementById('emailTextField').focus();
+        break;
+      default:
+        this.showInfoBox(error.message, DialogType.ERROR);
+        return;
+    }
+  }
+
+  showInfoBox(message, type=DialogType.DEFAULT, { description, page }={}) {
     const infoBoxDiv = document.getElementById('infoBoxDiv');
 
     switch (type) {
       case 1:
-        ReactDOM.render(<UI.WarningBox>{message}</UI.WarningBox>, infoBoxDiv);
+        ReactDOM.render(<UI.WarningBox>{message} {{ description, page } ? this.composeLink(description, page) : null}</UI.WarningBox>, infoBoxDiv);
         break;
       case 2:
-        ReactDOM.render(<UI.ErrorBox>{message}</UI.ErrorBox>, infoBoxDiv);
+        ReactDOM.render(<UI.ErrorBox>{message} {{ description, page } ? this.composeLink(description, page) : null}</UI.ErrorBox>, infoBoxDiv);
         break;
       default:
-        ReactDOM.render(<UI.InfoBox>{message}</UI.InfoBox>, infoBoxDiv);
+        ReactDOM.render(<UI.InfoBox>{message} {{ description, page } ? this.composeLink(description, page) : null}</UI.InfoBox>, infoBoxDiv);
     }
 
     infoBoxDiv.hidden = false;
+  }
+
+  composeLink(description, page) {
+    return (
+      <Router history={require('history').createBrowserHistory()}>
+        <Link to={page} onClick={() => this.props.history.push(page)}>{description}</Link>
+      </Router>
+    );
   }
 
   collapseErrorBox() {
@@ -176,4 +212,4 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp;
+export default withRouter(SignUp);
