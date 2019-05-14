a35e0dc927e867141a0d39d83b7f635a9054d048
tahscenery <tahscenery@gmail.com>
2019-05-14 13:22:33 +1000

    Added username text field

--- src/components/authentication/AccountDetails.js
+++ src/components/authentication/AccountDetails.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-// import fire from '../../config/Fire';
-// import AppDefaults from '../../AppDefaults';
+import fire from '../../config/Fire';
+import AppDefaults from '../../AppDefaults';
 import * as UI from '../../controls/UI';
 import './styles/AccountDetails.css';
 
 class AccountDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+    };
+  }
+
   render() {
+    const handleUsernameTextFieldChange = e => {
+      this.setState({ username: e.target.value })
+    }
+
     return (
       <div className="account-details-wrapper">
         <div className="account-details-form-container">
@@ -15,7 +27,10 @@ class AccountDetails extends Component {
             <UI.Heading className="account-details-heading">Almost there</UI.Heading>
             <UI.Subheading>Let's personalise your profile</UI.Subheading>
           </div>
-          <div id="infoBoxDiv" className="account-details-info-box-div" hidden={true}/>
+          <div className="account-details-form-group">
+            <UI.Label htmlFor="form-group">username</UI.Label>
+            <UI.TextField id="usernameTextField" name="username" placeholder="Username" value={this.state.username} onChange={handleUsernameTextFieldChange.bind(this)}/>
+          </div>
           <div className="account-details-button-group">
             <UI.Button id="backButton" className="back-button" onClick={() => this.props.history.goBack()}>Back</UI.Button>
             <UI.Button primary id="createAccountButton" className="create-account-button">Create Account</UI.Button>
