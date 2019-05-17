abd7d851450d58402cbba327d2be7002fad11cad
tahscenery <tahscenery@gmail.com>
2019-05-17 14:35:46 +1000

    Added biography text field

--- src/components/authentication/AccountDetails.js
+++ src/components/authentication/AccountDetails.js
@@ -11,13 +11,20 @@ class AccountDetails extends Component {
     super(props);
 
     this.state = {
-      username: ""
+      username: "",
+      bio: ""
     };
   }
 
   render() {
+    const continueButtonIsEnabled = this.state.username.length !== 0;
+
     const handleUsernameTextFieldChange = e => {
       this.setState({ username: e.target.value })
+    }
+
+    const handleBioTextAreaChange = e => {
+      this.setState({ bio: e.target.value });
     }
 
     return (
@@ -27,13 +34,19 @@ class AccountDetails extends Component {
             <UI.Heading className="account-details-heading">Almost there</UI.Heading>
             <UI.Subheading>Let's personalise your profile</UI.Subheading>
           </div>
+          <img className="account-icon" src="/assets/account_icon.png" alt="account_icon"/>
           <div className="account-details-form-group">
+            {/* <img src="/assets/account_icon.png" alt="account_icon" height="120" width="120"/> */}
             <UI.Label htmlFor="form-group">username</UI.Label>
             <UI.TextField id="usernameTextField" name="username" placeholder="Username" value={this.state.username} onChange={handleUsernameTextFieldChange.bind(this)}/>
           </div>
+          <div className="account-details-form-group">
+            <UI.Label htmlFor="form-group">bio</UI.Label>
+            <UI.TextField id="bioTextArea" name="bio" placeholder="Write a bit about yourself..." value={this.state.bio} onChange={handleBioTextAreaChange.bind(this)}/>
+          </div>
           <div className="account-details-button-group">
-            <UI.Button id="backButton" className="back-button" onClick={() => this.props.history.goBack()}>Back</UI.Button>
-            <UI.Button primary id="createAccountButton" className="create-account-button">Create Account</UI.Button>
+            {/* <UI.Button id="backButton" className="back-button" onClick={() => this.props.history.goBack()}>Back</UI.Button> */}
+            <UI.Button primary id="createAccountButton" className="create-account-button" disabled={!continueButtonIsEnabled}>Continue</UI.Button>
           </div>
         </div>
       </div>
