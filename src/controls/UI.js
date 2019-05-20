428113fe07c4a34d75d7f564de1976d5f35bf7a9
tahscenery <tahscenery@gmail.com>
2019-05-21 00:26:22 +1000

    Fixed exports

--- src/controls/UI.js
+++ src/controls/UI.js
@@ -1,24 +1,28 @@
 import { Title, Caption, Heading, Subheading, Label } from './Headings';
-
 import { Button, HintButton } from './Buttons';
-import { TextField } from './TextField';
-
+import { TextField, TextArea } from './TextField';
 import { InfoBox, WarningBox, ErrorBox, showInfoBox, DialogType } from './Dialogs';
 
 export {
+  // Headings
   Title,
   Caption,
   Heading,
   Subheading,
   Label,
 
+  // Buttons
   Button,
   HintButton,
-  TextField,
 
+  // Text Inputs
+  TextField,
+  TextArea,
+
+  // Dialogs
+  showInfoBox,
+  DialogType,
   InfoBox,
   WarningBox,
   ErrorBox,
-  showInfoBox,
-  DialogType
 };
