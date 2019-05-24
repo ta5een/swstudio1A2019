303f23fbd8395924602019fc9ece971834539c4c
tahscenery <tahscenery@gmail.com>
2019-05-25 01:34:24 +1000

    Renamed `AppDefaults` to `Globals`

---
+++ src/Globals.js
@@ -0,0 +1,33 @@
+const Globals = Object.freeze({
+  app: {
+    name: "Time Aid",
+    caption: "Your time spent on good causes",
+    version: "0.1.5"
+  },
+  constants: {
+    criteria: {
+      minPasswordLength: 8
+    },
+    styles: {
+      border: {
+        size: "1.5px",
+        radius: "5px"
+      },
+      font: {
+        family: {
+          default: "'Rubik', sans-serif"
+        },
+        sizes: {
+          small: "14px",
+          normal: "16px",
+          subheading: "20px",
+          heading: "30px",
+          caption: "30px",
+          title: "22px"
+        }
+      }
+    }
+  }
+});
+
+export default Globals;
