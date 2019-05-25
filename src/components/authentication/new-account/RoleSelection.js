281654f723af22f72c76ba9cdd06ca3aa23a9884
tahscenery <tahscenery@gmail.com>
2019-05-25 21:06:47 +1000

    Switched images to low-res versions

--- src/components/authentication/new-account/RoleSelection.js
+++ src/components/authentication/new-account/RoleSelection.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import fire from '../../../config/Fire';
 import * as UI from '../../../controls/UI';
 import './styles/RoleSelection.css';
 
 class RoleSelection extends Component {
   render() {
-    const handleSelection = () => {
-      console.log("I am clicked!");
-      this.props.history.push('/create-account');
+    const handleSelection = selection => {
+      this.props.history.push('/create-account', { selectedRole: selection });
     }
 
     return(
@@ -21,8 +19,8 @@ class RoleSelection extends Component {
             <UI.Subheading>Let's start by selecting your role</UI.Subheading>
           </div>
           <div className="role-selection-cards-container">
-            <UI.RoleCard image="/assets/low-res/volunteer.jpg" label="I'm a volunteer" description="I want to help make a difference in making the world a better place." onClick={handleSelection.bind(this)}/>
-            <UI.RoleCard image="/assets/low-res/charity_organiser.jpg" label="I'm a charity organiser" description="I want to create, organise, and hold events that enthusiastic patrons can participate in." onClick={handleSelection.bind(this)}/>
+            <UI.RoleCard image="/assets/low-res/volunteer.jpg" label="I'm a volunteer" description="I want to help make a difference in making the world a better place." onClick={handleSelection.bind(this, 'volunteer')}/>
+            <UI.RoleCard image="/assets/low-res/charity_organiser.jpg" label="I'm a charity organiser" description="I want to create, organise, and hold events that enthusiastic patrons can participate in." onClick={handleSelection.bind(this, 'charity')}/>
           </div>
         </div>
       </div>
