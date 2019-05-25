d2add66981b48019d62a0f569148167d129a84ed
tahscenery <tahscenery@gmail.com>
2019-05-25 21:06:23 +1000

    Fixed user registration

--- src/components/authentication/new-account/CreateAccount.js
+++ src/components/authentication/new-account/CreateAccount.js
@@ -6,13 +6,17 @@ import Globals from '../../../Globals';
 import * as UI from '../../../controls/UI';
 import './styles/CreateAccount.css';
 
-const minPasswordLength = Globals.constants.minPasswordLength;
+const minUsernameLength = Globals.constants.criteria.minUsernameLength;
+const minEmailLength = Globals.constants.criteria.minEmailLength;
+const minPasswordLength = Globals.constants.criteria.minPasswordLength;
 
 class CreateAccount extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      selectedRole: this.props.location.state.selectedRole,
+
       username: "",
       email: "",
       password: "",
@@ -21,40 +25,56 @@ class CreateAccount extends Component {
       errorUsername: false,
       errorEmail: false,
       errorPassword: false,
-      errorRepeatPassword: false
+      errorRepeatPassword: false,
+
+      formSubmittable: false
     };
 
-    this.handleNextButton = this.handleNextButton.bind(this);
+    this.handleCreateAccount = this.handleCreateAccount.bind(this);
   }
 
   componentDidMount() {
     document.title = `${Globals.app.name} – Sign Up`;
   }
 
-  handleNextButton(e) {
+  handleCreateAccount(e) {
     e.preventDefault();
+    document.getElementById('createAccountButton').blur();
 
-    document.getElementById('nextButton').blur();
+    const usernameTextField = document.getElementById('usernameTextField');
+    const emailTextField = document.getElementById('emailTextField');
+    const passwordTextField = document.getElementById('passwordTextField');
 
-    var usernameTextFieldLength = document.getElementById('usernameTextField').value.length;
-    var emailTextFieldLength = document.getElementById('emailTextField').value.length;
-
-    if (usernameTextFieldLength === 0) {
+    if (usernameTextField.value.length === 0) {
       UI.showInfoBox(this, "Please enter your username.", UI.DialogType.WARNING);
       this.setState({ errorUsername: true });
-      document.getElementById('usernameTextField').focus();
-    } else if (emailTextFieldLength === 0) {
+      usernameTextField.focus();
+
+    } else if (emailTextField.value.length === 0) {
       UI.showInfoBox(this, "Please enter your email.", UI.DialogType.WARNING);
       this.setState({ errorEmail: true });
-      document.getElementById('emailTextField').focus();
-    } else if (!this.passwordMeetsMinLength()) {
-      UI.showInfoBox(this, `Passwords must be at least ${minPasswordLength} characters.`, UI.DialogType.WARNING);
+      emailTextField.focus();
+
+    } else if (emailTextField.value.length < minEmailLength) {
+      UI.showInfoBox(this, "That doesn't look like a valid email, please try again.", UI.DialogType.WARNING);
+      this.setState({ errorEmail: true });
+      emailTextField.focus();
+
+    } else if (passwordTextField.value.length === 0) {
+      UI.showInfoBox(this, "All accounts must have a strong password with a minimum of 8 characters. Please try again.", UI.DialogType.WARNING);
+      this.setState({ errorPassword: true });
+      passwordTextField.focus();
+
+    } else if (passwordTextField.value.length < minPasswordLength) {
+      UI.showInfoBox(this, `Passwords must be a minimum of ${minPasswordLength} characters.`, UI.DialogType.WARNING);
       this.setState({ errorPassword: true, errorRepeatPassword: true });
-      document.getElementById('passwordTextField').focus();
+      passwordTextField.focus();
+
     } else if (!this.passwordsAreIdentical()) {
       UI.showInfoBox(this, "The passwords you have entered do not match. Please try again.", UI.DialogType.ERROR);
       this.setState({ errorPassword: true, errorRepeatPassword: true });
-      document.getElementById('repeatPasswordTextField').focus();
+      passwordTextField.focus();
+
     } else {
       this.commitCreateAccount();
     }
@@ -63,25 +83,34 @@ class CreateAccount extends Component {
   passwordsAreIdentical() {
     const passwordTextField = document.getElementById('passwordTextField');
     const repeatPasswordTextField = document.getElementById('repeatPasswordTextField');
-
-    return (passwordTextField.value !== "") && (passwordTextField.value === repeatPasswordTextField.value);
+    return (passwordTextField.value.length > 0) && (repeatPasswordTextField.value === passwordTextField.value);
   }
 
-  passwordsMeetMinLength() {
+  showIfPasswordsAreIdentical() {
+    if (!this.passwordsAreIdentical()) {
+      UI.showInfoBox(this, "Passwords do not match", UI.DialogType.WARNING, { description: "", page: "" });
+    } else {
+      if (document.getElementById('passwordTextField').value.length < minPasswordLength) {
+        UI.showInfoBox(this, "Passwords too short.", UI.DialogType.WARNING);
+      } else {
+        UI.showInfoBox(this, "Passwords match");
+      }
+    }
+  }
+
+  isFormSubmittable() {
+    const usernameTextField = document.getElementById('usernameTextField');
+    const emailTextField = document.getElementById('emailTextField');
     const passwordTextField = document.getElementById('passwordTextField');
-    const repeatPasswordTextField = document.getElementById('repeatPasswordTextField');
 
-    return (passwordTextField.value.length >= minPasswordLength) && (repeatPasswordTextField.value.length >= minPasswordLength);
-  }
+    if (usernameTextField && emailTextField && passwordTextField) {
+      return (usernameTextField.value.length >= minUsernameLength)
+            && (emailTextField.value.length >= minEmailLength)
+            && (passwordTextField.value.length >= minPasswordLength)
+            && this.passwordsAreIdentical();
+    }
 
-  passwordMeetsMinLength() {
-    const passwordTextField = document.getElementById('passwordTextField');
-    return passwordTextField.value.length >= minPasswordLength;
-  }
-
-  repeatPasswordMeetsMinLength() {
-    const repeatPasswordTextField = document.getElementById('repeatPasswordTextField');
-    return repeatPasswordTextField.value.length >= minPasswordLength;
+    return false;
   }
 
   commitCreateAccount() {
@@ -94,7 +123,7 @@ class CreateAccount extends Component {
       if (userExists) {
         UI.showInfoBox(this, "An account with the email you provided already exists.", UI.DialogType.ERROR, { description: "Forgot your password?", page: '/forgot-password' });
       } else {
-        this.props.history.push('/personalise-account', { email: this.state.email, password: this.state.password });
+        this.createUser();
       }
     }).catch(error => {
       this.displayAuthError(error);
@@ -106,12 +135,12 @@ class CreateAccount extends Component {
       case 'auth/invalid-email':
         UI.showInfoBox(this, "Hmm, that email doesn't look right. Check that you've entered it correctly and try again.", UI.DialogType.ERROR);
         this.setState({ errorEmail: true });
-        document.getElementById('emailTextField').focus();
+        this.emailTextField.focus();
         break;
       case 'auth/email-already-in-use':
         UI.showInfoBox(this, "An account with the email you provided already exists.", UI.DialogType.ERROR, { description: "Forgot your password?", page: '/forgot-password' });
         this.setState({ errorEmail: true });
-        document.getElementById('emailTextField').focus();
+        this.emailTextField.focus();
         break;
       default:
         UI.showInfoBox(this, error.message, UI.DialogType.ERROR);
@@ -119,21 +148,57 @@ class CreateAccount extends Component {
     }
   }
 
+  createUser() {
+    const displayInternalError = error => {
+      UI.showInfoBox(this, "An internal error occurred: " + error.message, UI.DialogType.ERROR);
+    }
+
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(userCredentials => {
+        console.log("1: Created user");
+        console.log("Current user: " + fire.auth().currentUser.uid);
+
+        const currentUser = userCredentials.user;
+        const usersDocRef = fire.firestore().doc('users/' + currentUser.uid);
+        const charitiesDocRef = fire.firestore().doc('charities/' + currentUser.uid);
+
+        currentUser.sendEmailVerification();
+        currentUser.updateProfile({ displayName: this.state.username })
+          .then(() => {
+            console.log("2: Updated profile with username");
+
+            if (this.state.selectedRole === 'volunteer') {
+              console.log("Volunteer");
+
+              usersDocRef.set({ name: this.state.username })
+                .then(() => {
+                  console.log("3: User Firestore properties set. Redirecting...");
+                  this.props.history.push('/account-created');
+                })
+                .catch(error => displayInternalError(error));
+            } else if (this.state.selectedRole === 'charity') {
+              console.log("Charity");
+
+              charitiesDocRef.set({ name: this.state.username })
+                .then(() => {
+                  console.log("3: Charity Firestore properties set. Redirecting...");
+                  this.props.history.push('/account-created');
+                })
+                .catch(error => displayInternalError(error));
+            }
+          })
+          .catch(error => displayInternalError(error));
+      })
+      .catch(error => displayInternalError(error));
+  }
+
   collapseErrorBox() {
     document.getElementById('infoBoxDiv').hidden = true;
     this.setState({ errorUsername: false, errorEmail: false, errorPassword: false, errorRepeatPassword: false });
   }
 
-  showIfPasswordsAreIdentical() {
-    if (!this.passwordsAreIdentical()) {
-      UI.showInfoBox(this, "Passwords do not match", UI.DialogType.WARNING, { description: "", page: "" });
-    } else {
-      UI.showInfoBox(this, "Passwords match");
-    }
-  }
-
   render() {
-    const nextButtonIsEnabled = this.state.email.length >= 3 && this.state.password.length >= minPasswordLength && this.passwordsAreIdentical();
+    const createAccountButtonIsEnabled = this.isFormSubmittable();
 
     const focusEmailTextField = e => {
       if (e.key === 'Enter') {
@@ -155,7 +220,7 @@ class CreateAccount extends Component {
 
     const submitForm = e => {
       if (e.key === 'Enter') {
-        this.handleNextButton(e);
+        this.handleCreateAccount(e);
       }
     }
 
@@ -209,7 +274,7 @@ class CreateAccount extends Component {
             <div id="infoBoxDiv" className="sign-up-info-box-div" hidden={true}/>
           </form>
           <div className="sign-up-create-button-container">
-            <UI.Button primary id="nextButton" type="submit" disabled={!nextButtonIsEnabled} onClick={this.handleNextButton}>CREATE ACCOUNT</UI.Button>
+            <UI.Button primary id="createAccountButton" type="submit" disabled={!createAccountButtonIsEnabled} onClick={this.handleCreateAccount}>CREATE ACCOUNT</UI.Button>
           </div>
         </div>
       </div>
