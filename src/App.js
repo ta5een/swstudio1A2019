d03d4fe3d59461b243a9cfc34a2768721da0a4b4
tahscenery <tahscenery@gmail.com>
2019-05-26 19:45:14 +1000

    Added API to get current user

--- src/App.js
+++ src/App.js
@@ -4,55 +4,51 @@ import { withRouter } from 'react-router-dom';
 import fire from './config/Fire';
 import './App.css';
 
+const wait = ms => new Promise(callback => setTimeout(callback, ms));
+
+const retryOperation = (fn, times, delay) => {
+  return new Promise((resolve, reject) => {
+    return fn()
+      .then(resolve)
+      .catch(reason => {
+        if (times - 1 > 0) {
+          return wait(delay)
+            .then(retryOperation.bind(null, fn, times - 1, delay))
+            .then(resolve)
+            .catch(reject);
+        }
+
+        return reject(reason);
+      });
+  })
+}
+
+const maximumAttempts = 10;
+
+const getCurrentUser = () => new Promise((resolved, rejected) => {
+  let currentUser = fire.auth().currentUser;
+  if (currentUser) {
+    console.log("Found user!");
+    return resolved(currentUser);
+  } else {
+    console.log("Unable to resolve current user. Trying again...");
+    return rejected("Timeout – Failed to get current user on time.");
+  }
+});
+
+export const attemptGetCurrentUser = (attempts=maximumAttempts) => {
+  return retryOperation(getCurrentUser, attempts, 1000);
+}
+
 class App extends Component {
   componentDidMount() {
-    this.attemptToGetCurrentUser();
-  }
-
-  attemptToGetCurrentUser() {
-    const wait = ms => new Promise(callback => setTimeout(callback, ms));
-
-    const retryOperation = (fn, times, delay) => {
-      return new Promise((resolve, reject) => {
-        return fn()
-          .then(resolve)
-          .catch(reason => {
-            if (times - 1 > 0) {
-              return wait(delay)
-                .then(retryOperation.bind(null, fn, times - 1, delay))
-                .then(resolve)
-                .catch(reject);
-            }
-
-            return reject(reason);
-          });
-      })
-    }
-
-    const getCurrentUser = () => new Promise((resolved, rejected) => {
-      let currentUser = fire.auth().currentUser;
-      if (currentUser) {
-        console.log("Found user!");
-        return resolved(currentUser);
-      } else {
-        console.log("Unable to resolve current user. Trying again...");
-        return rejected();
-      }
-    });
-
-    const maximumAttempts = 3;
-
-    retryOperation(getCurrentUser, maximumAttempts, 1000)
+    attemptGetCurrentUser()
       .then(user => {
         console.log("Currently signed-in user: " + user.uid);
-        this.setState({ user });
-        localStorage.setItem('user', user.uid);
         this.props.history.push('/home');
       })
-      .catch(() => {
-        console.log("Timeout: No user signed in");
-        this.setState({ user: null });
-        localStorage.removeItem('user');
+      .catch(error => {
+        console.log(error);
         this.props.history.push('/start');
       });
   }
--- src/components/main-interface/Home.js
+++ src/components/main-interface/Home.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import queryString from 'query-string';
 
 import fire from '../../config/Fire';
 import Globals from '../../Globals';
 import * as UI from '../../controls/UI';
 import './styles/Home.css';
 
+import { attemptGetCurrentUser } from '../../App';
+
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = ({
-      user: ""
+      showTour: queryString.parse(this.props.location.search).tour ? true : false,
+      user: null
     });
 
     this.logout = this.logout.bind(this);
@@ -19,13 +23,13 @@ class Home extends Component {
 
   componentDidMount() {
     document.title = `${Globals.app.name} – Home`;
-    const user = fire.auth().currentUser;
 
-    if (!user) {
-      this.props.history.push('/login');
-    } else {
-      this.setState({ user });
-    }
+    attemptGetCurrentUser()
+      .then(user => this.setState({ userResolved: true, user }))
+      .catch(error => {
+        console.log(error);
+        this.props.history.push('/login?kicked_out=yes');
+      })
   }
 
   logout() {
@@ -33,15 +37,33 @@ class Home extends Component {
     this.props.history.push('/start');
   }
 
+  loadingScreen() {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  cardScreen() {
+    return (
+      <div>
+        <p>Hi, {this.state.user.displayName}!</p>
+        <p>Signed in as <b>{this.state.user.email}</b>. Your uid is {this.state.user.uid}</p>
+        <UI.Button danger onClick={this.logout}>Logout</UI.Button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="home-wrapper">
+        <div className="home-title-bar">
+          <UI.TitleBar title="Home" hasSearchIcon={true}/>
+        </div>
         <div className="home-content">
-          <div className="home-header">
-            <UI.Heading>Home</UI.Heading>
-            <p>Hi, {this.state.user.displayName}!</p>
-            <p>Signed in as <b>{this.state.user.email}</b>, your uid is {this.state.user.uid}</p>
-            <UI.Button danger onClick={this.logout}>Logout</UI.Button>
+          <div className="home-cards">
+            {this.state.user ? this.cardScreen() : this.loadingScreen()}
           </div>
         </div>
       </div>
