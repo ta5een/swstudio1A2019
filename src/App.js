0d2e7ea2cd84d1b13e3f9fde051846254faa32bc
tahscenery <tahscenery@gmail.com>
2019-06-01 02:53:23 +1000

    Moved `attemptGetCurrentUser` to `Fire` config

--- src/App.js
+++ src/App.js
@@ -2,44 +2,8 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { PulseLoader } from 'react-spinners';
 
-import fire from './config/Fire';
+import { attemptGetCurrentUser } from './config/Fire';
 import './App.css';
-
-const wait = ms => new Promise(callback => setTimeout(callback, ms));
-
-const retryOperation = (fn, times, delay) => {
-  return new Promise((resolve, reject) => {
-    return fn()
-      .then(resolve)
-      .catch(reason => {
-        if (times - 1 > 0) {
-          return wait(delay)
-            .then(retryOperation.bind(null, fn, times - 1, delay))
-            .then(resolve)
-            .catch(reject);
-        }
-
-        return reject(reason);
-      });
-  })
-}
-
-const maximumAttempts = 5;
-
-const getCurrentUser = () => new Promise((resolved, rejected) => {
-  let currentUser = fire.auth().currentUser;
-  if (currentUser) {
-    console.log("Found user!");
-    return resolved(currentUser);
-  } else {
-    console.log("Unable to resolve current user. Trying again...");
-    return rejected("Timeout – Failed to get current user on time.");
-  }
-});
-
-export const attemptGetCurrentUser = (attempts=maximumAttempts) => {
-  return retryOperation(getCurrentUser, attempts, 1000);
-}
 
 class App extends Component {
   componentDidMount() {
--- src/config/Fire.js
+++ src/config/Fire.js
@@ -11,3 +11,38 @@ const config = {
 
 const fire = firebase.initializeApp(config);
 export default fire;
+
+const wait = ms => new Promise(callback => setTimeout(callback, ms));
+
+const retryOperation = (fn, times, delay) => {
+  return new Promise((resolve, reject) => {
+    return fn()
+      .then(resolve)
+      .catch(reason => {
+        if (times - 1 > 0) {
+          return wait(delay)
+            .then(retryOperation.bind(null, fn, times - 1, delay))
+            .then(resolve)
+            .catch(reject);
+        }
+
+        return reject(reason);
+      });
+  })
+}
+
+const maximumAttempts = 5;
+
+const getCurrentUser = () => new Promise((resolved, rejected) => {
+  let currentUser = fire.auth().currentUser;
+  if (currentUser) {
+    return resolved(currentUser);
+  } else {
+    console.log("Unable to resolve current user. Trying again...");
+    return rejected("Timeout – Failed to get current user on time.");
+  }
+});
+
+export const attemptGetCurrentUser = (attempts=maximumAttempts) => {
+  return retryOperation(getCurrentUser, attempts, 1000);
+}
