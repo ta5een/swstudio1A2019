911cacbe63ff665b4018dc9f440cee56f7d0d51d
tahscenery <tahscenery@gmail.com>
2019-06-02 06:02:08 +1000

    Center-aligned `PickerView` selected option

--- src/controls/PickerView.js
+++ src/controls/PickerView.js
@@ -8,6 +8,11 @@ const globalFontFamily = Globals.constants.styles.font.family;
 const globalBorderProps = Globals.constants.styles.border;
 
 const styles = css`
+  .picker-view-container {
+    display: block;
+    position: relative;
+  }
+
   .picker-view {
     display: block;
     max-height: 65px;
@@ -18,7 +23,7 @@ const styles = css`
     /* background-position: right 4px top 8px, right 4px bottom 8px; */
     background-position: center top, center bottom;
     background-repeat: no-repeat;
-    color: ${globalColours.grey.contrast};
+    color: rgba(0, 0, 0, 0);
 
     border: ${globalBorderProps.size} solid ${globalColours.grey.contrast};
     border-radius: ${globalBorderProps.radius};
@@ -32,21 +37,56 @@ const styles = css`
     -webkit-appearance: none;
     appearance: none;
   }
+
+  .picker-view p {
+    background: red;
+  }
+
+  .picker-view-text-container {
+    display: block;
+    position: absolute;
+
+    height: 100%;
+    width: 100%;
+
+    top: 48%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    background: none;
+
+    pointer-events: none;
+  }
+
+  .picker-view-text-container p {
+    font-family: ${globalFontFamily.default};
+    font-size: 20px;
+    font-weight: 500;
+
+    color: ${globalColours.grey.contrast};
+
+    text-align: center;
+  }
 `;
 
-export const PickerView = ({ id=null, ref=null, options=[], placeholder, value, onChange=null, flex=1 }) => {
+export const PickerView = ({ id=null, ref=null, options=[], value, onChange=null, flex=1 }) => {
   return (
     <>
       <style type="text/css">{styles}</style>
-      <select id={id} ref={ref} className="picker-view" name="picker-view" onChange={onChange} value={value}>
-        {options.map((option, index) => {
-          return (
-            <option value={option} key={`${index}-${new Date().getTime()}`}>
-              {option}
-            </option>
-          );
-        })}
-      </select>
+      <div className="picker-view-container">
+        <select id={id} ref={ref} className="picker-view" name="picker-view" onChange={onChange} value={value}>
+          {options.map((option, index) => {
+            return (
+              <option value={option} key={`${index}-${new Date().getTime()}`}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+        <div className="picker-view-text-container">
+          <p>{value}</p>
+        </div>
+      </div>
     </>
   );
 }
